e4c6eddaffbdf6560b6c611ec86a348aa1116fef
Jesse Talavera-Greenberg <jessetalavera@aol.com>
2017-03-29 16:54:00 -0400

    Improve benchmark-upx.sh

--- benchmark-upx.sh
+++ benchmark-upx.sh
@@ -4,16 +4,18 @@ source benchmark-common.sh
 
 UPX_CSV="upx-$DATE.csv"
 
-mkdir -p "llvm-bins-backup"
 echo "Downloading LLVM"
 wget "http://releases.llvm.org/4.0.0/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz"
 echo "Extracting LLVM"
 tar -xf "clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10.tar.xz"
-    find "./clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10" -type f -print \
-      | file --separator "" --files-from - \
-      | grep "x86-64" \
-      | cut --fields=1 --delimiter=" " \
-      | xargs -l -I '{}' cp {} "llvm-bins-backup"
+
+echo "Finding all executable files in LLVM..."
+mkdir -p "llvm-bins-backup"
+find "./clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-16.10" -type f -print \
+  | file --separator "" --files-from - \
+  | grep "x86-64" \
+  | cut --fields=1 --delimiter=" " \
+  | xargs -l -I '{}' cp {} "llvm-bins-backup"
 
 echo "Beginning compression of LLVM, with $TRIALS trials for each number of CPUs"
 echo "$CSV_HEADINGS" > "$UPX_CSV"
@@ -23,14 +25,17 @@ for i in $(seq $TOTAL_PROCESSORS); do
   for j in $(seq $TRIALS); do
     echo "    Trial $j"
     echo -n "$i, " >> "$UPX_CSV"
+    echo "      Copying binaries..."
     cp -r "llvm-bins-backup" "llvm-bins"
 
+    echo "      Compressing binaries..."  # This is the part that's timed
     /usr/bin/time \
       --output="$UPX_CSV" \
       --append \
       --format "$TIME_FORMAT" \
-      bash -c "ls bins | xargs --max-lines=1 --max-procs=$i upx --best --ultra-brute -qqq"
+      bash -c "ls llvm-bins | xargs --max-lines=1 --max-procs=$i -I '{}' upx --best --ultra-brute ./llvm-bins/{}"
 
+    echo "      Removing compressed binaries..."
     rm -rf "./llvm-bins"
   done
 done
